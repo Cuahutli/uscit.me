9ecbf94224f0ec5eb2eec3bfc4f7b3b6c1d28cf2
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-06-28 13:24:13 -0600

    se agregó un manager personalizados para el modelo shortener
    --> se sobreescribio el metodo all()
    --> se agregó un metodo personalizado que se puede llamar: KirrURL.objects.refresh_shortcodes() y sirve para refrescar los shortcodes ya existentes (cambiarlos)

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,12 +1,31 @@
 from django.db import models
 from .utils import create_shortcode
 # Create your models here.
+class KirrURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs = super(KirrURLManager, self).all(*args, **kwargs)
+        qs = qs.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = KirrURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q.shortcode)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i = new_codes) 
+
 
 class KirrURL(models.Model):
     url = models.CharField(max_length=220, )
     shortcode = models.CharField(max_length=15,  unique=True, blank=True)
     update = models.DateTimeField(auto_now=True) #everytime the model is saved
     timestamp = models.DateTimeField(auto_now_add=True) #when model whas created
+    active =  models.BooleanField(default=True)
+
+    objects = KirrURLManager()
     
     def save(self, *args, **kwargs):
         if self.shortcode is None or self.shortcode == "":

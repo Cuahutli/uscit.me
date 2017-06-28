2fe550d56a2188eb9e78b3d8119580104c06263f
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-06-28 13:05:40 -0600

    creando shortcode dinamicamente
    entendiendo como funciona el paso de una instancia como parámetro de una función

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,21 +1,16 @@
-import random
-import string
 from django.db import models
-
+from .utils import create_shortcode
 # Create your models here.
-
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
 
 class KirrURL(models.Model):
     url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15,  unique=True)
+    shortcode = models.CharField(max_length=15,  unique=True, blank=True)
     update = models.DateTimeField(auto_now=True) #everytime the model is saved
     timestamp = models.DateTimeField(auto_now_add=True) #when model whas created
     
     def save(self, *args, **kwargs):
-        self.shortcode = code_generator()
-        print(self.shortcode)
+        if self.shortcode is None or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
         super(KirrURL, self).save(*args, **kwargs)
 
     

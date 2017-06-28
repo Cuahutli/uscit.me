108e3d9e19300841168e28377c21fdf036a56171
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-06-28 12:48:45 -0600

    sobreescribimos el metodo save para que genere automáticamente vía una función code_generator() el código de la url

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,6 +1,11 @@
+import random
+import string
 from django.db import models
 
 # Create your models here.
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 class KirrURL(models.Model):
     url = models.CharField(max_length=220, )
@@ -8,7 +13,12 @@ class KirrURL(models.Model):
     update = models.DateTimeField(auto_now=True) #everytime the model is saved
     timestamp = models.DateTimeField(auto_now_add=True) #when model whas created
     
+    def save(self, *args, **kwargs):
+        self.shortcode = code_generator()
+        print(self.shortcode)
+        super(KirrURL, self).save(*args, **kwargs)
 
+    
     def __str__(self):
         return str(self.url)
 

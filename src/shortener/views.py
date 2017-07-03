e601c39eef5ad742d83f9dba81810a6d4e54b281
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-03 14:34:25 -0600

    prueba de CBV vs FBV

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def kirr_redirect_view(request): #Function Based View FBV
+    return HttpResponse("Hello")
+
+class KirrCBView(View): #Class Based View CBV
+    def get(self, request, *args, **kwargs): 
+        return HttpResponse("hellos Again")

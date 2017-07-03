2fdbeee17f5a739c142efde6c92aa9d690e33b6c
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-03 15:17:05 -0600

    consultar el shortcode en la base de datos y traer su informacion

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,16 +1,39 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .models import KirrURL
 # Create your views here.
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs): #Function Based View FBV
-    #print(request.user)
-    #print(request.user.is_authenticated())
-    #print(args)
-    #print(kwargs)
-    print(shortcode)
-    return HttpResponse("Hello {sc}".format(sc=shortcode))
+    obj = get_object_or_404(KirrURL, shortcode=shortcode)
+    return HttpResponse("Hello {sc}".format(sc=obj.url))
 
 class KirrCBView(View): #Class Based View CBV
-    def get(self, request, shortcode=None, *args, **kwargs): 
-        return HttpResponse("Hello again {sc}".format(sc=shortcode))
+    def get(self, request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(KirrURL, shortcode=shortcode) 
+        return HttpResponse("Hello again {sc}".format(sc=obj.url))
+
+    #def post(self,request, *args, **kwargs):
+    #    return HttpResponse()
+
+
+'''
+def kirr_redirect_view(request, shortcode=None, *args, **kwargs): #Function Based View FBV
+    
+    # esta es la mejor manera de las 3
+    obj = get_object_or_404(KirrURL, shortcode=shortcode)
+    # forma 1
+    #try:
+    #    obj = KirrURL.objects.get(shortcode=shortcode)
+    #except:
+    #    obj = KirrURL.objects.all().first()
+
+    # forma 2 esto es mejor que el bloque try
+    #obj_url = None
+    #qs = KirrURL.objects.filter(shortcode__iexact=shortcode.upper())
+    #if qs.exists and qs.count() == 1:
+    #    obj = qs.first()
+    #    obj_url = obj.url
+
+    return HttpResponse("Hello {sc}".format(sc=obj.url))
+'''

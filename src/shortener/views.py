33addd871aaaf070d16f6d1d81e4d63a87bcd5a2
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-05 15:38:16 -0600

    implementando django forms en lugar del form convencional

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -3,14 +3,29 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from .models import KirrURL
+from .forms import SubmitURLForm
 # Create your views here.
 
 class HomeView(View):
+    
+
     def get(self, request, *args, **kwargs):
-        return render(request, "shortener/home.html", {}) 
+        form = SubmitURLForm()
+        ctx = {
+            "title": "Envía la URL",
+            "form": form
+        }
+        return render(request, "shortener/home.html", ctx) 
 
     def post(self, request, *args, **kwargs):
-        return render(request, "shortener/home.html", {}) 
+        form = SubmitURLForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        ctx = {
+            "title": "Envía la URL",
+            "form": form
+        }
+        return render(request, "shortener/home.html", ctx) 
 
 class KirrCBView(View): #Class Based View CBV
     def get(self, request, shortcode=None, *args, **kwargs):
